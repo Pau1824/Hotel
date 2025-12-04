32dc19db5259a2767161d6d11e8058c3de443551
Pau1824 <chaviraespinozapaulina@gmail.com>
2025-12-04 00:48:46 -0700

    puerto localhost

--- sistema/backend/src/server.js
+++ sistema/backend/src/server.js
@@ -1,9 +1,8 @@
 import app from './app.js';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor backend escuchando en http://localhost:${PORT}`);
+  console.log(`Servidor backend escuchando en puerto ${PORT}`);
 });
+
