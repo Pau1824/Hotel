85b13d481782999b45372467191d3b728c6867b7
Pau1824 <chaviraespinozapaulina@gmail.com>
2025-12-04 20:52:47 -0700

    db

--- sistema/backend/src/db.js
+++ sistema/backend/src/db.js
@@ -2,14 +2,14 @@ import pkg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'Hotel',
-  password: process.env.DB_PASS || '18052004pau',
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
